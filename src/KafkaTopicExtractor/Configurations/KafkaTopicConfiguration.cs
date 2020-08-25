34e2194cc52685d72252ffcee2e84f80f46a63d5
Alex <alexander.g@btigroup.io>
2020-08-25 16:54:06 +0300

    Using enum converter for config

--- src/KafkaTopicExtractor/Configurations/KafkaTopicConfiguration.cs
+++ src/KafkaTopicExtractor/Configurations/KafkaTopicConfiguration.cs
@@ -1,10 +1,15 @@
-﻿namespace KafkaTopicExtractor.Configurations
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace KafkaTopicExtractor.Configurations
 {
     public class KafkaTopicConfiguration
     {
         public string[] Brokers { get; set; } = new string[0];
         public string Topic { get; set; }
         public uint EventsToRead { get; set; } = 0; // infinite
+        
+        [JsonConverter(typeof(StringEnumConverter))]
         public OffsetKind OffsetKind { get; set; } = OffsetKind.Latest;
     }
 
