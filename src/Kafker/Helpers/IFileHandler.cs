713cad4d5c170c0930b2dfa9665878d49e23d8ba
Ivana Raykova <ivana.r@btigroup.io>
2020-10-15 16:22:09 +0300

    Introduced IFileHandler responsible for loading/saving files

---
+++ src/Kafker/Helpers/IFileHandler.cs
@@ -0,0 +1,15 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using Confluent.Kafka;
+
+namespace Kafker.Helpers
+{
+    public interface IFileHandler
+    {
+        Task SaveToFileAsync(FileInfo destinationCsvFile, Timestamp messageTimeStamp, string messageValue);
+        Task<IEnumerable<string>> LoadFromFileAsync(string sourceFile);
+    }
+}
