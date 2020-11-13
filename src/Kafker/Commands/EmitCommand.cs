584ac1cbc00daf65efc3793af89b0d8b2ec58068
ivana <ivana.r@btigroup.io>
2020-11-13 20:21:40 +0200

    Refactored preserved time logic

--- src/Kafker/Commands/EmitCommand.cs
+++ src/Kafker/Commands/EmitCommand.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.IO;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Kafker.Configurations;
@@ -27,59 +29,53 @@ namespace Kafker.Commands
         {
             var cfg = await ExtractorHelper.ReadConfigurationAsync(topic, _settings, _console);
 
-            try
-            {
-                var allLines = await File.ReadAllLinesAsync(fileName, cancellationToken);
-                var a = new List<Tuple<long, string>>();
-                var b = new List<Tuple<long, string>>();
-                var c = new Dictionary<long, List<string>>();
-                foreach (var item in allLines)
-                {
-                    var pair = item.Split("|");
-                    a.Add(new Tuple<long, string>(long.Parse(pair[0].Replace("\"", "")), pair[1]));
-                }
-
-                long smallest = a[0].Item1;
-                for (int i = 1; i < a.Count; i++)
-                {
-                    if (smallest > a[i].Item1)
-                    {
-                        smallest = a[i].Item1;
-                    }
-                }
-
-                for (int i = 0; i < a.Count; i++)
-                {
-                    b.Add(new Tuple<long, string>(a[i].Item1 - smallest, a[i].Item2));
-                }
-
-
-                foreach (var item in b)
-                {
-                    if (!c.ContainsKey(item.Item1))
-                    {
-                        c.Add(item.Item1, new List<string>());
-                    }
-                    c[item.Item1].Add(item.Item2);
-                }
-
-                var list = new List<Timer>();
-                foreach (var item in c)
-                {
-                    var jsons = item.Value;
-                    var times = item.Key;
-                    list.Add(new Timer(SendJsons,jsons,times,Timeout.Infinite));
-                }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-            }
-
+            //var shouldPreserve = preserve != false;
+            var shouldPreserve = false;
+            var timerList = new List<Timer>();
             var producedEvents = 0;
             using var topicProducer = _producerFactory.Create(cfg);
-            using (var reader = new StreamReader(fileName))
+            if (!shouldPreserve)
             {
+                try
+                {
+                    var getDictionary = await CreatePreserveTime(fileName);
+                    using var stateObject = new PreserveTime();
+                    var waitHandles = new List<WaitHandle>();
+                    foreach (var item in getDictionary)
+                    {
+                        if (cancellationToken.IsCancellationRequested) break;
+
+                        stateObject.ItemsToSend = item.Value;
+                        stateObject.ProducerConfig = cfg;
+                        stateObject.ResetEvent = new AutoResetEvent(false);
+                        waitHandles.Add(stateObject.ResetEvent);
+                        var delay = item.Key;
+                        timerList.Add(new Timer(EmitEventsOnTime, stateObject, delay, Timeout.Infinite));
+                        producedEvents++;
+                    }
+
+                    WaitHandle.WaitAll(waitHandles.ToArray());
+
+                    int a = 0;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    throw;
+                }
+                finally
+                {
+                    foreach (var item in timerList)
+                    {
+                        await item.DisposeAsync();
+                    }
+
+                    await _console.Out.WriteLineAsync($"\r\nProduced {producedEvents} events");
+                }
+            }
+            else
+            {
+                using var reader = new StreamReader(fileName);
                 try
                 {
                     string line;
@@ -88,11 +84,14 @@ namespace Kafker.Commands
                         if (cancellationToken.IsCancellationRequested) break;
 
                         var pair = line.Split("|");
-                        //var timestamp = pair[0].Substring(1, pair[0].Length - 2);
                         var jsonText = pair[1].Substring(1, pair[1].Length - 2);
                         await topicProducer.ProduceAsync(jsonText);
                         producedEvents++;
                     }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
                 }
                 finally
                 {
@@ -103,18 +102,57 @@ namespace Kafker.Commands
             return await Task.FromResult(0).ConfigureAwait(false); // ok
         }
 
-        private void SendJsons(object state)
+        private void EmitEventsOnTime(object state)
         {
             if (state != null)
             {
-                var jsonList = (List<string>)state;
-                Parallel.ForEach(jsonList, s =>
-                {
-                    //produce
-                    // topicProducer.ProduceAsync(jsonText).GetAwaiter().GetResult();
-                    
-                });
+                var preserver = (PreserveTime) state;
                 
+                using var topicProducer = _producerFactory.Create(preserver.ProducerConfig);
+                foreach (var item in preserver.ItemsToSend)
+                {
+                    topicProducer.ProduceAsync(item).GetAwaiter().GetResult();
+                }
+
+                preserver.ResetEvent.Set();
+            }
+        }
+
+        private async Task<Dictionary<long, List<string>>> CreatePreserveTime(string fileName)
+        {
+            var allLines = await File.ReadAllLinesAsync(fileName);
+            var timeStampWithEvents = new List<Tuple<long, string>>();
+            var eventsToEmit = new Dictionary<long, List<string>>();
+            var initialSnapshot = allLines.Select(item => item.Split("|")).Select(pair => new Tuple<long, string>(long.Parse(pair[0].Replace("\"", "")), pair[1])).ToList();
+            var smallest = initialSnapshot.Select(x => x.Item1).Min();
+
+            foreach (var (timeStamp, itemToSend) in initialSnapshot)
+            {
+                timeStampWithEvents.Add(new Tuple<long, string>(timeStamp - smallest, itemToSend));
+            }
+
+            foreach (var (timeStamp, itemToSend) in timeStampWithEvents)
+            {
+                if (!eventsToEmit.ContainsKey(timeStamp))
+                {
+                    eventsToEmit.Add(timeStamp, new List<string>());
+                }
+
+                eventsToEmit[timeStamp].Add(itemToSend);
+            }
+
+            return eventsToEmit;
+        }
+
+        private class PreserveTime : IDisposable
+        {
+            public List<string> ItemsToSend { get; set; }
+            public AutoResetEvent ResetEvent { get; set; }
+            public KafkaTopicConfiguration ProducerConfig { get; set; }
+
+            public void Dispose()
+            {
+                ResetEvent?.Dispose();
             }
         }
     }
